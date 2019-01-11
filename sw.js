9bb34d59cc605ec575819ff2fc5be6931425a84c
eedamame <eedamame@gmail.com>
2019-01-11 20:55:29 +0900

    テキスト更新

--- sw.js
+++ sw.js
@@ -2,10 +2,6 @@ importScripts('/pwa_test/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/pwa_test/_nuxt/3b894cb35d66a119c3d9.js",
-    "revision": "85744eda42f0fab59015056aae737787"
-  },
-  {
     "url": "/pwa_test/_nuxt/47fad98ccae235574044.js",
     "revision": "336f0c55058affe63e8a1e8799fb83db"
   },
@@ -14,8 +10,12 @@ workbox.precaching.precacheAndRoute([
     "revision": "44a80d37f24838fcb67204e34dd4f7e0"
   },
   {
-    "url": "/pwa_test/_nuxt/d9d4222f2a13df030318.js",
-    "revision": "2340212a468d7f929f17fc602971d2ae"
+    "url": "/pwa_test/_nuxt/bf81a42352c9e38430dd.js",
+    "revision": "9d07a7ae2cb1861620a96e998d811b2a"
+  },
+  {
+    "url": "/pwa_test/_nuxt/f6bdc6370c3736e5d115.js",
+    "revision": "e0071fdd3391040b0769fc8c15d3d33a"
   }
 ], {
   "cacheId": "pwa_test",
