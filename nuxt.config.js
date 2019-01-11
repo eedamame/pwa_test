a73235f366c5e20dfa1900efc679c1401162cff8
eedamame <eedamame@gmail.com>
2019-01-11 20:17:20 +0900

    configからworkbox消してみた

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,12 +38,12 @@ module.exports = {
   modules: [
     '@nuxtjs/pwa',
   ],
-  workbox: {
-    dev: true, //開発環境でもPWAできるように
-  },
   manifest: {
     name: 'sample nuxt pwa',
     lang: 'ja'
   },
+  //workbox: {
+  //  dev: true, //開発環境でもPWAできるように
+  //},
 }
 
